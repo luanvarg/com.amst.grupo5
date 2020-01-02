24f4dd6d84a31dd587059b2b29ddbd7fb5b2cc0e
Luis Vargas <luanvarg@espol.edu.ec>
2020-01-01 22:50:58 -0500

    Commit inicial

---
+++ app/src/main/java/com/example/comamstgrupo5/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.comamstgrupo5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void abrirVistaRegistros(View view){
+        Intent intent= new Intent(this, Registros.class);
+     startActivity(intent);
+    }
+}
